f3711cf9babf156b1a5d27fb8e6097213f2cb6ee
Nirmal <nirmalr2512@gmail.com>
2023-02-12 15:34:11 +0530

    🧹Logo Image Url updated

--- res/misc/readme.go
+++ res/misc/readme.go
@@ -2,7 +2,7 @@ package misc
 
 const MiscReadme = `
 <p align="center">
-  <a href="http://go-niom.com" target="blank"><img src="https://github.com/go-niom/niom/master/img/niom_logo.png" width="320" alt="Niom Logo" /></a>
+  <a href="http://go-niom.com" target="blank"><img src="https://raw.githubusercontent.com/go-niom/niom/master/img/niom_logo.png" width="320" alt="Niom Logo" /></a>
 </p>
 
 <p align="center">Niom is a <a href="https://github.com/nestjs/nest" target="_blank">Nestjs</a> inspired progressive <a href="https://go.dev" target="_blank">Golang</a> web framework for developing efficient and scalable server-side applications. Designed to ease things up for fast development </p>
