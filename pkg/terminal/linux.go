05d1cff9872b263b7f39e30d8c7ee6ffb39bc4d8
Nirmal <nirmalr2512@gmail.com>
2023-05-19 23:18:24 +0530

    so specific terminal file added

---
+++ pkg/terminal/linux.go
@@ -0,0 +1,85 @@
+//go:build linux
+// +build linux
+
+package terminal
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+	"syscall"
+
+	"github.com/go-niom/niom/pkg/logger"
+)
+
+// KillFunc kills the shell started by the Execute
+func KillFunc() error {
+
+	PID, err := syscall.Getpgid(cmd.Process.Pid)
+	if err == nil {
+		logger.Warn("Killing App runner")
+		if errKill := syscall.Kill(-PID, syscall.SIGTERM); errKill != nil {
+			return errKill
+		}
+	}
+	if PID < 0 && !isUserExit {
+		go TermCmd.Execute()
+	}
+
+	return nil
+}
+
+// Execute executes shell command
+func (term *TerminalCmd) Execute() error {
+	//check command has args
+	if len(term.Args) > 0 {
+		cmd = exec.Command(term.App, term.Args...)
+	} else {
+		cmd = exec.Command(term.App)
+	}
+
+	if cmd != nil {
+		cmd.Dir = term.Dir
+	}
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	if err := cmd.Start(); err != nil {
+		log.Printf("Error executing command: %s\n", err.Error())
+		return err
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		io.Copy(os.Stdout, stdout)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		io.Copy(os.Stderr, stderr)
+		reader := bufio.NewReader(stdout)
+		line, err := reader.ReadString('\n')
+		for err == nil {
+			fmt.Println(line)
+			line, err = reader.ReadString('\n')
+		}
+	}()
+
+	wg.Wait()
+	if err := cmd.Wait(); err != nil {
+		return term.cmdErrors(err)
+	}
+	if term.ShowMessage {
+		logger.Info("Command Executed Successfully")
+	}
+	return nil
+}
--- pkg/terminal/terminal.go
+++ pkg/terminal/terminal.go
@@ -1,15 +1,9 @@
 package terminal
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
 	"os"
 	"os/exec"
 	"os/signal"
-	"runtime"
-	"sync"
 	"syscall"
 
 	"github.com/go-niom/niom/pkg/logger"
@@ -28,35 +22,6 @@ type TerminalCmd struct {
 	App         string
 	Args        []string
 	ShowMessage bool
-}
-
-// KillFunc kills the shell started by the Execute
-// KillFunc kills the shell started by the Execute
-func KillFunc() error {
-	if runtime.GOOS != "windows" {
-		if cmd.ProcessState == nil {
-			logger.Warn("Killing App runner")
-			if errKill := cmd.Process.Kill(); errKill != nil {
-				return errKill
-			}
-		}
-
-		if cmd.ProcessState != nil && !isUserExit {
-			go TermCmd.Execute()
-		}
-	} else {
-		PID, err := syscall.Getpgid(cmd.Process.Pid)
-		if err == nil {
-			logger.Warn("Killing App runner")
-			if errKill := syscall.Kill(-PID, syscall.SIGTERM); errKill != nil {
-				return errKill
-			}
-		}
-		if PID < 0 && !isUserExit {
-			go TermCmd.Execute()
-		}
-	}
-	return nil
 }
 
 // to check user cancel event cmd+c
@@ -89,57 +54,6 @@ func (term *TerminalCmd) cmdErrors(err error) error {
 		return err
 	}
 	return err
-}
-
-// Execute executes shell command
-func (term *TerminalCmd) Execute() error {
-	//check command has args
-	if len(term.Args) > 0 {
-		cmd = exec.Command(term.App, term.Args...)
-	} else {
-		cmd = exec.Command(term.App)
-	}
-
-	if cmd != nil {
-		cmd.Dir = term.Dir
-	}
-	stdout, _ := cmd.StdoutPipe()
-	stderr, _ := cmd.StderrPipe()
-
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	if err := cmd.Start(); err != nil {
-		log.Printf("Error executing command: %s\n", err.Error())
-		return err
-	}
-
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		io.Copy(os.Stdout, stdout)
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		io.Copy(os.Stderr, stderr)
-		reader := bufio.NewReader(stdout)
-		line, err := reader.ReadString('\n')
-		for err == nil {
-			fmt.Println(line)
-			line, err = reader.ReadString('\n')
-		}
-	}()
-
-	wg.Wait()
-	if err := cmd.Wait(); err != nil {
-		return term.cmdErrors(err)
-	}
-	if term.ShowMessage {
-		logger.Info("Command Executed Successfully")
-	}
-	return nil
 }
 
 // CmdExecute trigger the Execute function
